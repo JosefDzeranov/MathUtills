7a8bfb2959f05cc0b2f96229293be1f25db56aaf
Unknown <soslan29@gmail.com>
2017-08-21 22:20:00 +0300

    Add project

---
+++ MathHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtills
+{
+    public static class MathHelper
+    {
+        // Returns the square of a right triangle.
+        public static double GetRightTriangleSquare(double a, double b, double c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                throw new InvalidOperationException("Triangle does not exist.");
+            }
+
+            if (a * a + b * b == c * c)
+            {
+                return a * b / 2;
+            }
+
+            if (a * a + c * c == b * b)
+            {
+                return a * c / 2;
+            }
+
+            if (c * c + b * b == a * a)
+            {
+                return c * b / 2;
+            }
+
+            throw new InvalidOperationException("Triangle is not right.");
+        }
+    }
+}
---
+++ MathUtills.UnitTests/MathHelperTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathUtills;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MathUtills.UnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class MathHelperTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetRightTriangleSquare_TriangleNotExist_ThrowException_Test()
+        {
+            double a = 1;
+            double b = 3;
+            double c = 4;
+
+            MathHelper.GetRightTriangleSquare(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetRightTriangleSquare_TriangleNotRight_ThrowException_Test()
+        {
+            double a = 2;
+            double b = 5;
+            double c = 6;
+
+            MathHelper.GetRightTriangleSquare(a, b, c);
+        }
+        
+        [TestMethod]
+        public void GetRightTriangleSquare_GetTriangleSquareFirstCase_ThrowException_Test()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+
+            var actual = MathHelper.GetRightTriangleSquare(a, b, c);
+
+            Assert.AreEqual(6, actual,"Invalid square.");
+        }
+
+        [TestMethod]
+        public void GetRightTriangleSquare_GetTriangleSquareSecondCase_ThrowException_Test()
+        {
+            double a = 3;
+            double b = 5;
+            double c = 4;
+
+            var actual = MathHelper.GetRightTriangleSquare(a, b, c);
+
+            Assert.AreEqual(6, actual, "Invalid square.");
+        }
+       
+        [TestMethod]
+        public void GetRightTriangleSquare_GetTriangleSquareThirdCase_ThrowException_Test()
+        {
+            double a = 5;
+            double b = 3;
+            double c = 4;
+
+            var actual = MathHelper.GetRightTriangleSquare(a, b, c);
+
+            Assert.AreEqual(6, actual, "Invalid square.");
+        }
+    }
+}
